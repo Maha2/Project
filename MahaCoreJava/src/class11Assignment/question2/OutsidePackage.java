04fbe9f571dbe9e6de5db7b63ef104d0b71a2914
Krishna <krishna@indikya.com>
2016-04-26 14:49:14 -0700

    I commit the 2nd question.

---
+++ MahaCoreJava/src/class11Assignment/question2/OutsidePackage.java
@@ -0,0 +1,15 @@
+package class11Assignment.question2;
+
+import class11Assignment.question1.Student;
+
+public class OutsidePackage {
+
+	public static void main(String[] args) {
+		Student stuMaha = new Student();
+		stuMaha.stuName = "Maha";
+		stuMaha.stuId = 1;
+		stuMaha.stuGrade = "Second";
+
+	}
+
+}
