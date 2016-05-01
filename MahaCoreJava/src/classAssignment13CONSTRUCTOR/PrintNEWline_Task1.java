c72cc3b601d3fc7b66ef27da4baa299a5ec87816
Krishna <krishna@indikya.com>
2016-04-30 17:14:14 -0700

    Task1 : output is printing to new line. Task2: swap this(...) along with name = nameValue;

---
+++ MahaCoreJava/src/classAssignment13CONSTRUCTOR/PrintNEWline_Task1.java
@@ -0,0 +1,13 @@
+package classAssignment13CONSTRUCTOR;
+
+public class PrintNEWline_Task1 {
+	public static void main(String[] args) {
+		DoctorType_Task2 maha = new DoctorType_Task2(1,"Maha ","Dentist");
+		DoctorType_Task2 amala = new DoctorType_Task2(2,"Amala", "heart surgeon");
+		DoctorType_Task2 akhil = new DoctorType_Task2(3, "Akhil","Dentist");
+		maha.printDetails();
+		amala.printDetails();
+		akhil.printDetails();
+	}
+
+}
