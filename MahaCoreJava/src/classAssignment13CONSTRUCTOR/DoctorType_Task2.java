fb78d35ad7e05d1c9b2517d2b170802fc0153834
Krishna <krishna@indikya.com>
2016-04-30 17:17:53 -0700

    Task1: output is printing to new line. Task 2:swap this(...) along with name = nameValue;

--- MahaCoreJava/src/classAssignment13CONSTRUCTOR/DoctorType_Task2.java
+++ MahaCoreJava/src/classAssignment13CONSTRUCTOR/DoctorType_Task2.java
@@ -1,31 +1,33 @@
 package classAssignment13CONSTRUCTOR;
 
 public class DoctorType_Task2 {
-	
+
 	public int id;
 	public String name;
 	public String designation;
-	
-	public DoctorType_Task2(){
-		
+
+	public DoctorType_Task2() {
+
 	}
-	
-	public DoctorType_Task2(int id,String name){
+
+	public DoctorType_Task2(int id, String name) {
 		this.id = id;
 		this.name = name;
-		
+
 	}
-	
-	public DoctorType_Task2(int id,String name,String designation){
-		//this.designation = designation;   ----- This statement is not possible to write first line in this Block. 
-		this(id,name);
-	    this.designation = designation;
-		
+
+	public DoctorType_Task2(int id, String name, String designation) {
+		// this.designation = designation; ----- This statement is not possible
+		// to write first line in this Block.
+		this(id, name);
+		this.designation = designation;
+
 	}
-	
-	public void printDetails(){
-		System.out.println("Doctor Id: "+id+"\nDoctor Name:"+name+"\nDoctor Designation: "+designation+"\n******************");
+
+	public void printDetails() {
+		System.out.println("Doctor Id: " + id + "\nDoctor Name:" + name
+				+ "\nDoctor Designation: " + designation
+				+ "\n******************");
 	}
-	
 
 }
